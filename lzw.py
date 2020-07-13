eb2fa787782c559e0829ab21ed5466864572b844
FoFabien <f.fabien@hotmail.fr>
2020-07-13 12:50:01 +0000

    removed the ugly str() calls

--- lzw.py
+++ lzw.py
@@ -11,22 +11,22 @@ import math
 
 def compress(uncompressed): # take a bytearray, output an integer list
     max_size = 4096
-    dictionary = {str([i]): i for i in range(256)} # self explanatory
+    dictionary = {chr(i): i for i in range(256)} # self explanatory
 
-    w = [uncompressed[0]] # first chara
+    w = chr(uncompressed[0]) # first chara
     result = []
     for i in range(1, len(uncompressed)): # start reading
-        c = uncompressed[i] # current chara
-        wc = w + [c] # check for previous chain + current chara
-        if str(wc) in dictionary: # already in dict, we'll check for the next chara
+        c = chr(uncompressed[i]) # current chara
+        wc = w + c # check for previous chain + current chara
+        if wc in dictionary: # already in dict, we'll check for the next chara
             w = wc
         else:
-            result.append(dictionary[str(w)]) # add to output the previous chain
+            result.append(dictionary[w]) # add to output the previous chain
             if len(dictionary) < max_size:
-                dictionary[str(wc)] = len(dictionary) # add full chain to dict if not full
-            w = [c] # previous chain set to current chara
+                dictionary[wc] = len(dictionary) # add full chain to dict if not full
+            w = c # previous chain set to current chara
     if len(w) > 0:
-        result.append(dictionary[str(w)]) # append the rest
+        result.append(dictionary[w]) # append the rest
     return result
 
 def compress_file(i):
@@ -39,25 +39,25 @@ def compress_file_to(i, o):
             bit_size = 9
             max_size = 512
             size_limit = 12
-            dictionary = {str([i]): i for i in range(max_size)}
+            dictionary = {chr(i): i for i in range(max_size)}
 
             buf = "" # buffer to handle our output and its weird width
             w = None
             for uncompressed in iter(partial(fi.read, 1024), b''):
                 for c in uncompressed:
                     if w is None:
-                        w = [c]
+                        w = chr(c)
                         continue
-                    wc = w + [c]
-                    if str(wc) in dictionary:
+                    wc = w + chr(c)
+                    if wc in dictionary:
                         w = wc
                     else:
-                        buf = buf + "{0:b}".format(dictionary[str(w)]).zfill(bit_size)
+                        buf = buf + "{0:b}".format(dictionary[w]).zfill(bit_size)
                         if len(buf) > 64:
                             fo.write(int(buf[0:64], 2).to_bytes(8, 'big'))
                             buf = buf[64:]
                         if len(dictionary) < max_size:
-                            dictionary[str(wc)] = len(dictionary)
+                            dictionary[wc] = len(dictionary)
                             if len(dictionary) == max_size:
                                 if bit_size < size_limit:
                                     bit_size += 1
@@ -66,9 +66,9 @@ def compress_file_to(i, o):
                                     bit_size = 9
                                     max_size = 512
                                     dictionary = {str([i]): i for i in range(max_size)}
-                        w = [c]
+                        w = chr(c)
             if len(w) > 0:
-                buf = buf + "{0:b}".format(dictionary[str(w)]).zfill(bit_size) # add the leftover
+                buf = buf + "{0:b}".format(dictionary[w]).zfill(bit_size) # add the leftover
                 while len(buf) % 8 != 0:
                     buf += "0" # padding
                 l = ((len(buf) - 1) // 8) + 1
@@ -76,21 +76,21 @@ def compress_file_to(i, o):
 
 def decompress(compressed): # take an integer list (first element being the bit size), output a bytearray
     max_size = 4096
-    dictionary = [[i] for i in range(256)]
+    dictionary = [chr(i) for i in range(256)]
 
     result = bytearray()
-    entry = []
-    w = []
+    entry = ""
+    w = ""
     for c in compressed:
         if c < len(dictionary):
             entry = dictionary[c]
         elif c == len(dictionary):
-            entry = w + [w[0]]
+            entry = w + w[0]
         else:
             raise ValueError('Bad compressed c: %s' % c)
-        for e in entry: result.append(e)
+        for e in entry: result.append(ord(e))
         if len(w) > 0 and len(dictionary) < max_size:
-            dictionary.append(w + [entry[0]])
+            dictionary.append(w + entry[0])
         w = entry
     return result
 
@@ -104,11 +104,11 @@ def decompress_file_to(i, o):
             bit_size = 9
             max_size = 512
             size_limit = 12
-            dictionary = {i: [i] for i in range(max_size)}
+            dictionary = {i: chr(i) for i in range(max_size)}
 
             result = bytearray()
-            entry = []
-            w = []
+            entry = ""
+            w = ""
 
             buf = ""
             for chunk in iter(partial(fi.read, 64), b''):
@@ -119,12 +119,12 @@ def decompress_file_to(i, o):
                     if c in dictionary:
                         entry = dictionary[c]
                     elif c == len(dictionary):
-                        entry = w + [w[0]]
+                        entry = w + w[0]
                     else:
                         raise ValueError('Bad compressed c: %s' % c)
-                    for e in entry: result.append(e)
+                    for e in entry: result.append(ord(e))
                     if len(w) > 0 and len(dictionary) < max_size:
-                        dictionary[len(dictionary)] = w + [entry[0]]
+                        dictionary[len(dictionary)] = w + chr(entry[0])
                         if len(dictionary) == max_size:
                             if bit_size < size_limit:
                                 bit_size += 1
@@ -141,7 +141,9 @@ if __name__ == "__main__":
     with open("in.txt", "w") as f:
         f.write("Hello world !!\nThis is a test to study LZW algorithm!\nabcdefghijklmnopqrstuvwxyz\n123456789\n987654321\n0000")
     print("Compressing...")
-    compress_file_to("in.txt", "compressed")
+    compress_file_to("in.odt", "compressed")
     print("Decompressing...")
-    decompress_file_to("compressed", "out.txt")
+    decompress_file_to("compressed", "out.odt")
     print("Done")
+
+    #print(decompress(compress("hello world!!".encode('ascii'))))
